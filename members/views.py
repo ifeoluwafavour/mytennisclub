7aa8b8793f5a6477553f80c45fd56ad2af44fdbe
Ife <64481442+ifeoluwafavour@users.noreply.github.com>
2023-04-20 16:11:56 +0100

    Add files via upload

---
+++ members/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Members
+
+def members(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('allmembers.html')
+    context = {'mymembers': mymembers}
+    return HttpResponse(template.render(context, request))
+def details(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = { 'mymember': mymember }
+    return HttpResponse(template.render(context, request))
+def main(request):
+    template = loader.get_template("main.html")
+    return HttpResponse(template.render())
+def template(request):
+    template = loader.get_template("template.html")
+    return HttpResponse(template.render())
